4be755833f6a8dcabd57827d7a09e52548cf8359
wood </>
2019-03-23 13:47:52 -0700

    WIP

--- bin/jasmine.js
+++ bin/jasmine.js
@@ -4,8 +4,8 @@ var path = require('path'),
     Command = require('../lib/command.js'),
     Jasmine = require('../lib/jasmine.js');
 
-var jasmine = new Jasmine({ projectBaseDir: path.resolve() });
+var newJasmine = function () { return new Jasmine({ projectBaseDir: path.resolve() }) };
 var examplesDir = path.join(path.dirname(require.resolve('jasmine-core')), 'jasmine-core', 'example', 'node_example');
 var command = new Command(path.resolve(), examplesDir, console.log);
 
-command.run(jasmine, process.argv.slice(2));
+command.run(newJasmine, process.argv.slice(2));
